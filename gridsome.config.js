5a356ed787c1c5326807aab65e9cbba1afcab9b1
Frank Steinberg <franks@inmotionhosting.com>
2019-05-19 19:51:42 -0400

    Add Tailwind CSS and PurgeCSS

--- gridsome.config.js
+++ gridsome.config.js
@@ -3,8 +3,23 @@
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
+const purgecss = require('@fullhuman/postcss-purgecss')
+const tailwind = require('tailwindcss')
+
+const postcssPlugins = [
+  tailwind(),
+]
+
+if (process.env.NODE_ENV === 'production') postcssPlugins.push(purgecss())
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
+  siteName: 'Park Finder',
+  plugins: [],
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: postcssPlugins,
+      },
+    },
+  },
 }

389108c60e1df08805c9fbbd578ba98313972c15
Frank Steinberg <franks@inmotionhosting.com>
2019-05-19 20:09:20 -0400

    Create layout

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,14 +1,18 @@
 <template>
-  <div class="layout bg-green-200">
-    <header class="header">
-      <strong>
-        <g-link to="/">{{ $static.metaData.siteName }}</g-link>
-      </strong>
-      <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
+  <div class="layout flex flex-col min-h-screen bg-green-200 justify-between">
+    <header class="header border-t-8 border-teal-200 bg-teal-500">
+      <nav class="container mx-auto flex justify-between items-center py-4">
+        <g-link to="/" class="text-white text-4xl font-bold no-underline hover:border hover:border-white">{{ $static.metaData.siteName }}</g-link>
       </nav>
     </header>
-    <slot/>
+    <div class="flex-grow">
+      <slot />
+    </div>
+    <footer class="bg-teal-700 text-white h-16 p-2">
+      <div class="container mx-auto flex items-center justify-between py-4">
+        <div>Copyright 2019. All rights reserved.</div>
+      </div>
+    </footer>
   </div>
 </template>
 
